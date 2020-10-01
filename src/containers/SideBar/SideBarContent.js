ef7210ba26410490a9db7c04b640743deb436c18
Jean <jpere547@fiu.edu>
2020-10-01 11:31:59 -0400

    Skeleton Navigation complete

--- src/containers/SideBar/SideBarContent.js
+++ src/containers/SideBar/SideBarContent.js
@@ -5,6 +5,48 @@ import Navigation from "../../components/Navigation";
 const SideBarContent = () => {
   const navigationMenus = [
     {
+      name: "pages.banditgym",
+      type: "section",
+      children: [
+        {
+          name: "pages.banditgym.homepage",
+          icon: "home",
+          type: "item",
+          link: "/app/bandit-gym/Home",
+        },
+        {
+          name: "pages.banditgym.shoppage",
+          icon: "store",
+          type: "item",
+          link: "/app/bandit-gym/Shop",
+        },
+        {
+          name: "pages.banditgym.userProfile",
+          icon: "account",
+          type: "item",
+          link: "/app/bandit-gym/MyProfile",
+        },
+        {
+          name: "pages.banditgym.communityPage",
+          icon: "accounts-alt",
+          type: "item",
+          link: "/app/bandit-gym/BanditCommunity",
+        },
+        {
+          name: "pages.banditgym.supportpage",
+          icon: "pin-help",
+          type: "item",
+          link: "/app/bandit-gym/Support",
+        },
+        {
+          name: "pages.banditgym.aboutuspage",
+          icon: "book",
+          type: "item",
+          link: "/app/bandit-gym/AboutUs",
+        },
+      ],
+    },
+    {
       name: "pages.adminDashboard",
       type: "section",
       children: [
